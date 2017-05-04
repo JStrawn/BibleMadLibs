82405f53963ebb9bcd07f6e261fcff14a2490bd0
Max Schneider <mschn0405@gmail.com>
2017-05-04 18:40:23 -0400

    Changes and adding game screen controller

---
+++ BibleMadLibs/BibleMadLibs/TextEntryScreen.swift
@@ -0,0 +1,35 @@
+//
+//  TextEntryScreen.swift
+//  BibleMadLibs
+//
+//  Created by Maxwell Schneider on 5/4/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+import UIKit
+
+class TextEntryScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
