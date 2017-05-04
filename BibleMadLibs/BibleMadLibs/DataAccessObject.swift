88011b05ffcab38b35bfef0a5ef8cb80615b8a43
Andy Wu <andywu@Andys-Mac.local>
2017-05-04 14:57:50 -0400

    Added DAO File

---
+++ BibleMadLibs/BibleMadLibs/DataAccessObject.swift
@@ -0,0 +1,17 @@
+//
+//  DataAccessObject.swift
+//  BibleMadLibs
+//
+//  Created by Andy Wu on 5/4/17.
+//  Copyright © 2017 JStrawn. All rights reserved.
+//
+
+import Foundation
+
+
+class DataAccessObject {
+    static let sharedManager = DataAccessObject()
+    
+    
+    
+}
