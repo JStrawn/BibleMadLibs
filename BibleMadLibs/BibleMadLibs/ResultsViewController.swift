b49a629c1d59e9dab5e9c6dbdf439afb055401ef
Juliana Strawn <julianastrawn@gmail.com>
2017-05-04 22:25:30 -0400

    mad libs is now functional

--- BibleMadLibs/BibleMadLibs/ResultsViewController.swift
+++ BibleMadLibs/BibleMadLibs/ResultsViewController.swift
@@ -9,25 +9,32 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
     let mysharedManager = DataAccessObject.sharedManager
     
     var userWords = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let passage = mysharedManager.passage
+        
+        var passage = mysharedManager.passage
         print(passage!)
         
         
-//        if let match = passage!.range(of: "(?<=<)[^>]+", options: .regularExpression) {
-//            print(passage!.substring(with: match))
-//            passage!.replacingOccurrences(of: match, with: <#T##String#>)
-//        }
         
-
+        for word in userWords {
+            if let match = passage!.range(of: "\\<(.*?)\\>", options: .regularExpression) {
+                passage!.replaceSubrange(match, with: word)
+            }
+            
+        }
+        
+        var editedPassage = passage!.replacingOccurrences(of: " .", with: ".")
+        editedPassage = editedPassage.replacingOccurrences(of: " ,", with: ",")
+        editedPassage = editedPassage.replacingOccurrences(of: " :", with: ":")
+        print(editedPassage)
+        
     }
-
-
+    
+    
 }
